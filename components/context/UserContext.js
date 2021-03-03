761a5caeed1d3837bb8114e8ac78fb92a4c2900a
burrodecarga <edwinhenriquezh@hotmail.com>
2021-03-03 08:20:40 -0400

    1er commit

---
+++ components/context/UserContext.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import { useEffect } from 'react'
+
+const Context = React.createContext({})
+
+export function UserContextProvider ({children}) {
+  const [jwt, setJWT] = useState(
+    () => window.sessionStorage.getItem('jwt')
+  )
+
+  
+  return <Context.Provider value={{
+    jwt,
+    setJWT
+  }}>
+    {children}
+  </Context.Provider>
+}
+
+export default Context
